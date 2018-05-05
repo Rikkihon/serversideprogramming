cd6b29659b2f39ff4bd6610fab414ee375dfb22c
Rikki Honnold <rikki@er-10-129-143-5.wireless.umn.edu>
2018-05-05 13:13:24 -0500

    adding stuff to git from class

---
+++ index.js
@@ -0,0 +1,12 @@
+ //get val from form field using a script 
+ // store that variable
+ //initialize the server
+ //create the routes
+ //do work in the route
+ //show new page to user 
+//http://javascript-coder.com/javascript-form/javascript-get-form.phtml
+
+let input = oformElements.name;
+
+
+ 
